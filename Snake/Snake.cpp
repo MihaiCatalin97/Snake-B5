f8d171e16a4c781ed99e21f098c9b8938c0107eb
androbianca <biaanca.9805@yahoo.com>
2016-12-15 15:19:40 +0200

    Creare meniu
    Creare butoane si titlu

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -2,19 +2,24 @@
 #include "Snake.h"
 
 #define MAX_LOADSTRING 100
+#define			BUTTON_IDENTIFIER	1
 
+int nCmdShowG;
 HINSTANCE instanta_curenta;                                
 WCHAR titlu_fereastra[] = L"                          Main Menu";                  
 WCHAR nume_clasa_fereastra[] = L"Fereastra_Principala";           
 HWND fereastra_meniu;
+HWND startsingleplayer, startdoubleplayer, scor, exitbutton;
 
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 void                Initializare(HINSTANCE, int);
 LRESULT CALLBACK    Procesare_Comenzi_Fereastra(HWND, UINT, WPARAM, LPARAM);
+HWND Creare_Buton(WCHAR text_buton[], int x, int y, int inaltime, int latime);
+HWND fereastra_joc;
 
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-
+	nCmdShowG = nCmdShow;
     MyRegisterClass(hInstance);
 
 	Initializare(hInstance, nCmdShow);
@@ -49,7 +54,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SNAKE));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = CreateSolidBrush(RGB(210, 210, 210)); // coloreaza fereastra gri
+    wcex.hbrBackground  = CreateSolidBrush(RGB(255, 255, 255)); // coloreaza fereastra gri
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_SNAKE);
     wcex.lpszClassName  = nume_clasa_fereastra;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -70,8 +75,35 @@ void Initializare(HINSTANCE hInstance, int nCmdShow)
       desktop.right/2 - 150, desktop.bottom/2 - 200, 300, 400, nullptr, nullptr, hInstance, nullptr); // creeaza fereastra pe mijlocul ecranului
    SetMenu(fereastra_meniu, NULL); // elimina meniul de sus
 
+   startsingleplayer = Creare_Buton(L"Single Player", 75, 60, 50, 150);
+
+   startdoubleplayer = Creare_Buton(L"Double Player", 75, 130, 50, 150);
+
+   scor = Creare_Buton(L"Scor", 75, 200, 50, 150);
+
+   exitbutton = Creare_Buton(L"Exit", 75, 270, 50, 150);
+
    ShowWindow(fereastra_meniu, nCmdShow);
    UpdateWindow(fereastra_meniu);
+   afisarejoc();
+}
+
+HWND Creare_Buton(WCHAR text_buton[], int x, int y, int inaltime, int latime)
+{
+	HWND button = CreateWindow(
+		L"BUTTON",  // Predefined class; Unicode assumed 
+		text_buton,      // Button text 
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+		x,         // x position 
+		y,         // y position 
+		latime,        // Button width
+		inaltime,        // Button height
+		fereastra_meniu,     // Parent window
+		(HMENU)BUTTON_IDENTIFIER,
+		(HINSTANCE)GetWindowLong(fereastra_meniu, GWL_HINSTANCE),
+		NULL);      // Pointer not needed.
+
+	return button;
 }
 
 LRESULT CALLBACK Procesare_Comenzi_Fereastra(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -82,9 +114,29 @@ LRESULT CALLBACK Procesare_Comenzi_Fereastra(HWND hWnd, UINT message, WPARAM wPa
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
+			WCHAR text[] =L"Snake!";
+			int iY = 20;
+				TextOut(hdc, 125, iY, text, 6);
+		
             EndPaint(hWnd, &ps);
         }
         break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case BUTTON_IDENTIFIER:
+		{
+			//Step 5: User click on the button
+			if (HIWORD(wParam) == BN_CLICKED)
+			{
+				afisarejoc();
+			}
+		}
+		break;
+		}
+	}
+	break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -93,3 +145,18 @@ LRESULT CALLBACK Procesare_Comenzi_Fereastra(HWND hWnd, UINT message, WPARAM wPa
     }
     return 0;
 }
+
+void afisarejoc()
+{
+	RECT desktop;
+
+	const HWND hDesktop = GetDesktopWindow();
+	GetWindowRect(hDesktop, &desktop);
+
+	fereastra_joc = CreateWindow(nume_clasa_fereastra, titlu_fereastra, WS_SYSMENU,
+		desktop.right / 2 - 150, desktop.bottom / 2 - 200, 300, 400, nullptr, nullptr, instanta_curenta, nullptr);
+
+	ShowWindow(fereastra_joc, nCmdShowG);
+	UpdateWindow(fereastra_joc);
+}
+  
