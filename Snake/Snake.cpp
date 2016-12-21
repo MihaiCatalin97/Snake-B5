a0bad68e4a8cb20a1be261465dc82d892aaa30a1
androbianca <biaanca.9805@yahoo.com>
2016-12-21 13:38:42 +0200

    Editare fereastra de scor
    Afisarea scorului si customizarea ferestrei

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,14 +1,31 @@
 #include "stdafx.h"
 #include "Snake.h"
 #include "Meniu.h"
-
+#include <fstream>
+#include <string.h>
 using namespace Snake;
+using namespace std;
+
+void inserare();
+void sortare();
+void citire();
+
+//void AfisareScore();
 
 HINSTANCE Instanta_Curenta;
+
+
+int Scor;
+string Nume;
+
 
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
 	Instanta_Curenta = hInstance;
+	citire();
+	sortare();
+	
+	
 
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
@@ -25,6 +42,41 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLi
 			DispatchMessage(&msg);
 		}
 	}
-
 	return (int)msg.wParam;
+}
+
+
+void inserare()
+{
+	ofstream g("Scor.txt", std::ios_base::app | std::ios_base::out);
+	g << Nume << " " << Scor << "\n";
+
+
+}
+
+void citire()
+{
+	ifstream g("Scor.txt");
+
+	while (!g.eof() && dimensiune<100)
+	{
+		g >> Scoruri[dimensiune].Nume;
+		g >> Scoruri[dimensiune].Scor;
+		dimensiune++;
+	}
+
+}
+
+void sortare()
+{
+	int i, j;
+	inregistrare aux;
+	for (i = 0; i<dimensiune; i++)
+	for (j = i + 1; j<dimensiune; j++)
+	if (Scoruri[i].Scor<Scoruri[j].Scor)
+	{
+		aux = Scoruri[i];
+		Scoruri[i] = Scoruri[j];
+		Scoruri[j] = aux;
+	}
 }
--- Snake/Snake.h
+++ Snake/Snake.h
@@ -1,3 +1,14 @@
 #pragma once
 
 #include "resource.h"
+#include <string>
+
+using namespace std;
+
+struct inregistrare{
+	std::string Nume;
+	int Scor = 0;
+};
+
+inregistrare Scoruri[100];
+int dimensiune = 0;
